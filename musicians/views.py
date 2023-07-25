ed4923d6a8dcf19ab23ddac15b3d4e7f90c0a0af
Daniel Quinn <dandidit.quinn@gmail.com>
2023-07-25 16:18:49 -0700

    postgres

--- musicians/views.py
+++ musicians/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from .serializer import MusicianSerializer
 from .models import Musician
+from .permissions import IsOwnerOrReadOnly
 
 class MusicianList(generics.ListCreateAPIView):
+    permission_classes = (IsOwnerOrReadOnly, )
     serializer_class = MusicianSerializer
     queryset = Musician.objects.all()
 
 class MusicianDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly, )
     serializer_class = MusicianSerializer
     queryset = Musician.objects.all()
